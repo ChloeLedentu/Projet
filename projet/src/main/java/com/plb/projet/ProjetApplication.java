edb518b5d5471e6dd3c99abc440e4251af67161a
ChloeLedentu <60890370+ChloeLedentu@users.noreply.github.com>
2022-10-07 17:14:15 +0200

    Update ProjetApplication.java

--- projet/src/main/java/com/plb/projet/ProjetApplication.java
+++ projet/src/main/java/com/plb/projet/ProjetApplication.java
@@ -1,11 +1,6 @@
 package com.plb.projet;
 
-import java.time.LocalDate;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.StandardEnvironment;
 public class ProjetApplication {
